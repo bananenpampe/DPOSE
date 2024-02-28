ded4dc1885d7aca869b365c19a916d823b550928
Matthias Kellner <bananenpampe@t-online.de>
2024-02-28 03:34:47 +0100

    adds QM9 train

---
+++ Atomistic_experiments/training/QM9/mse_ens/5/training_QM9.py
@@ -0,0 +1,178 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "..",  "model"))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "..",   "utils"))
+
+import wandb
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+from rascaline_trainer_uncertainty_QM9 import BPNNRascalineModule
+from load import load_PBE0_TS
+from dataset.dataset import create_rascaline_dataloader
+from dataset.dataset_helpers import get_global_unique_species
+import rascaline
+import rascaline.torch
+import torch
+import ase.io
+import torch._dynamo
+import traceback as tb
+import random
+
+from transformer.composition import CompositionTransformer
+from pytorch_lightning.callbacks import LearningRateMonitor
+
+
+#default type is float64
+torch.set_default_dtype(torch.float64)
+
+# --- load the data ---
+
+SEED = 5
+random.seed(SEED)
+torch.manual_seed(SEED)
+
+
+# select a subset of the frames
+frames_train = ase.io.read("/home/kellner/BPNN_packages/H2O/data/qm9_train.xyz",":")[:20000]
+frames_val = ase.io.read("/home/kellner/BPNN_packages/H2O/data/qm9_val.xyz",":")[:2000]
+frames_test = ase.io.read("/home/kellner/BPNN_packages/H2O/data/qm9_test.xyz",":")
+
+
+
+# --- define the hypers ---
+hypers_ps = {
+    "cutoff": 5.,
+    "max_radial": 9,
+    "max_angular": 9,
+    "atomic_gaussian_width": 0.3,
+    "center_atom_weight": 0.0,
+    "radial_basis": {
+        "Gto": {},
+    },
+    "cutoff_function": {
+        "ShiftedCosine": {"width":0.5},
+    },
+    "radial_scaling":{"Willatt2018": {"exponent": 7.0, "rate": 1.0, "scale": 2.0}}
+}
+
+hypers_rs = {
+    "cutoff": 5.,
+    "max_radial": 16,
+    "atomic_gaussian_width": 0.3,
+    "center_atom_weight": 0.0,
+    "radial_basis": {
+        "Gto": {},
+    },
+    "cutoff_function": {
+        "ShiftedCosine": {"width":0.5},
+    },
+    "radial_scaling":{"Willatt2018": {"exponent": 7.0, "rate": 1.0, "scale": 2.0}}
+}
+
+
+# --- define calculator ---
+calc_rs = rascaline.torch.SoapRadialSpectrum(**hypers_rs)
+calc_ps = rascaline.torch.SoapPowerSpectrum(**hypers_ps)
+
+# --- create the dataloader ---
+dataloader_init = create_rascaline_dataloader(frames_train,
+                                         energy_key="U0",                                       
+                                         calculators=[calc_rs],
+                                         do_gradients=False,
+                                         precompute = True,
+                                         lazy_fill_up = False,
+                                         batch_size=len(frames_train), 
+                                         shuffle=True)
+
+dataloader = create_rascaline_dataloader(frames_train,
+                                         energy_key="U0",                                       
+                                         calculators=[calc_rs, calc_ps],
+                                         do_gradients=False,
+                                         precompute = True,
+                                         lazy_fill_up = False,
+                                         batch_size=32, 
+                                         shuffle=True)
+
+dataloader_val = create_rascaline_dataloader(frames_val,
+                                         energy_key="U0",                                       
+                                         calculators=[calc_rs, calc_ps],
+                                         do_gradients=False,
+                                         precompute = True,
+                                         lazy_fill_up = False,
+                                         batch_size=len(frames_val), 
+                                         shuffle=False)
+
+dataloader_test = create_rascaline_dataloader(frames_test,
+                                         energy_key="U0",                                       
+                                         calculators=[calc_rs, calc_ps],
+                                         do_gradients=False,
+                                         precompute = True,
+                                         lazy_fill_up = False,
+                                         batch_size=len(frames_test), 
+                                         shuffle=False)
+
+# --- create the trainer ---
+# for now a batch of features is necessary 
+# for correct weight init
+
+#COPY YOUR WANDB API KEY HERE, or load it fromn a file
+
+#read wandb api code from file
+wandb_api_key = "YOUR_API"
+
+wandb.login(key=wandb_api_key)
+wandb_logger = WandbLogger(name=str(SEED), project="QM9-mse-ensemble", log_model=True)
+wandb_logger.experiment.config["key"] = wandb_api_key
+
+# log the descriptor hyperparameters
+wandb_logger.log_hyperparams({"hypers radial spectrum": hypers_rs})
+wandb_logger.log_hyperparams({"hypers power spectrum": hypers_ps})
+
+print("seed", SEED)
+
+feat, prop, syst = next(iter(dataloader_init))
+
+transformer_e = CompositionTransformer()
+transformer_e.fit(syst, prop)
+
+
+
+
+
+all_spec = dataloader_init.dataset.all_species
+
+expected_keys = []
+for spec in all_spec:
+    expected_keys.append("LabelsEntry(species_center={})".format(spec))
+
+expected_keys = set(expected_keys)
+
+for n, (feat, _, _) in enumerate(dataloader):
+    tmp_keys = []
+    for key, t_block in feat.items():
+        tmp_keys.append(str(key))    
+    if set(tmp_keys) == expected_keys:
+        NO_FULL = False
+        break 
+
+# define the trainer
+module = BPNNRascalineModule(feat, transformer_e)
+
+#compiled_model = torch.compile(module,fullgraph=True )
+lr_monitor = LearningRateMonitor(logging_interval='epoch')
+
+trainer = Trainer(max_epochs=500,
+                  precision=64,
+                  accelerator="cpu",
+                  logger=wandb_logger,
+                  callbacks=[lr_monitor],
+                  gradient_clip_val=0.5,
+                  enable_progress_bar=False,
+                  val_check_interval=1.0,
+                  check_val_every_n_epoch=1,
+                  inference_mode=False)
+                  #profiler="simple")
+
+trainer.fit(module, dataloader, dataloader_val)
+trainer.test(module, dataloaders=dataloader_test)
---
+++ Atomistic_experiments/training/QM9/shallow_ens_crps/rascaline_trainer_uncertainty_QM9.py
@@ -0,0 +1,202 @@
+import torch
+import pytorch_lightning as pl
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "..", "model"))
+
+from metatensor.torch import TensorBlock, Labels, TensorMap
+from nn.model import BPNNModel
+from nn.interaction import BPNNInteraction
+from nn.aggregation import BPNNStructureWiseAggregationVar
+from nn.response import ForceUncertaintyRespone, UnitResponse
+from nn.loss import EnergyForceLoss, GeneralLossUQ, EnergyForceUncertaintyLoss, CRPS
+from transformer.composition import CompositionTransformer
+
+
+class BPNNRascalineModule(pl.LightningModule):
+    
+    def __init__(self,
+                 example_tensormap,
+                 energy_transformer=CompositionTransformer(),
+                 model = BPNNModel(
+                 interaction=BPNNInteraction(n_out=64, 
+                                             n_hidden= 256,
+                                             n_hidden_layers = 3,
+                                             activation = torch.nn.SiLU,),
+                 aggregation=BPNNStructureWiseAggregationVar(),
+                 response=UnitResponse()
+                 ),
+                 loss_fn = GeneralLossUQ,
+                 regularization=1e-03,
+                 w_force_uncertainty=False):
+        
+        super().__init__()
+        #print(regularization)
+        #self.save_hyperparameters({'l2 reg': regularization})
+        self.model = model
+        self.model.initialize_weights(example_tensormap)
+        
+        self.loss_fn = loss_fn(base_loss=CRPS)
+
+        self.loss_rmse = EnergyForceLoss(w_forces=False,
+                                base_loss=torch.nn.MSELoss)
+        
+        self.loss_mae = EnergyForceLoss(w_forces=False,
+                                base_loss=torch.nn.L1Loss)
+        
+        self.energy_transformer = energy_transformer
+        self.regularization = regularization
+
+    def forward(self, feats, systems):
+        return self.model(feats, systems)
+    
+    def calculate(self, feats, systems):
+        
+        outputs = self(feats, systems)
+
+        outputs = self.energy_transformer.inverse_transform(systems, outputs)
+
+        return outputs
+    
+
+    def training_step(self, batch, batch_idx):
+        feats, properties, systems = batch
+
+        batch_size = len(systems)
+
+        #print(feats.block(0).values.requires_grad)
+
+        properties = self.energy_transformer.transform(systems, properties)
+
+        outputs = self(feats,systems)
+        loss = self.loss_fn(outputs, properties)
+
+        self.log('train_loss', loss, enable_graph=True, batch_size=batch_size)
+        
+        """
+        for param in self.parameters():
+            loss += self.regularization * torch.norm(param)**2
+        """
+
+        return loss
+
+    def on_validation_model_eval(self, *args, **kwargs):
+        super().on_validation_model_eval(*args, **kwargs)
+        torch.set_grad_enabled(True)
+        
+    def on_test_model_eval(self, *args, **kwargs):
+        super().on_test_model_eval(*args, **kwargs)
+        torch.set_grad_enabled(True)
+
+    def validation_step(self, batch, batch_idx):
+
+        feats, properties, systems = batch 
+
+        batch_size = len(systems)
+
+        outputs = self(feats, systems)
+        outputs = TensorMap(properties.keys,[outputs.block(0).copy()])
+
+        outputs = self.energy_transformer.inverse_transform(systems, outputs)
+
+        energy_val_mse, forces_val_mse = self.loss_rmse.report(outputs, properties)
+        energy_val_mae, forces_val_mae = self.loss_mae.report(outputs, properties)
+
+        loss = energy_val_mse + forces_val_mse
+
+        self.log('val_loss',
+                 loss.item(),
+                 batch_size=batch_size,
+                 on_epoch=True)
+        
+        self.log("val_energy_mae",
+                 torch.clone(energy_val_mae),
+                 batch_size=batch_size,
+                 on_epoch=True)
+        
+        
+        self.log("val_energy_mse",
+                 torch.clone(energy_val_mse),
+                 batch_size=batch_size,
+                 on_epoch=True)
+
+        # log rmse
+        self.log("val_energy_rmse",
+                 torch.sqrt(torch.clone(energy_val_mse)),
+                 batch_size=batch_size,
+                 on_epoch=True)
+
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        
+        feats, properties, systems = batch
+
+
+        batch_size = len(systems)
+
+        outputs = self(feats, systems)
+        #for now:
+
+        outputs = TensorMap(properties.keys,[outputs.block(0)])
+
+        outputs = self.energy_transformer.inverse_transform(systems, outputs)
+
+        energy_test_mse, forces_test_mse = self.loss_rmse.report(outputs, properties)
+        energy_test_mae, forces_test_mae = self.loss_mae.report(outputs, properties)
+
+        loss = energy_test_mse + forces_test_mse
+
+        self.log('test_loss',
+                loss.item(),
+                batch_size=batch_size,
+                on_epoch=True)
+
+        self.log("test_energy_mae",
+                torch.clone(energy_test_mae),
+                batch_size=batch_size,
+                on_epoch=True)
+        
+        self.log("test_energy_mse",
+                torch.clone(energy_test_mse),
+                batch_size=batch_size,
+                on_epoch=True)
+
+        # log rmse
+        self.log("test_energy_rmse",
+                torch.sqrt(torch.clone(energy_test_mse)),
+                batch_size=batch_size,
+                on_epoch=True)
+        
+
+        return loss
+
+    def backward(self, loss, *args, **kwargs):
+        loss.backward(retain_graph=True)
+
+    """ 
+    def configure_optimizers(self):
+        optimizer = torch.optim.LBFGS(self.parameters(), lr=1., line_search_fn="strong_wolfe")
+        return optimizer
+    """
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(),lr=1e-3, amsgrad=True)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                                        patience=20,
+                                                                        factor=0.5,
+                                                                        verbose=True,
+                                                                        min_lr=1e-05),
+                "monitor": "val_energy_mae",
+                "interval": "epoch",
+                "frequency": 1
+                # If "monitor" references validation metrics, then "frequency" should be set to a
+                # multiple of "trainer.check_val_every_n_epoch".
+            },
+        }
